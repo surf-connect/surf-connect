0320b2b0060b8e4db92921822ec634ab7fdbc13f
charlie-robison <charlierobison480@gmail.com>
2021-11-13 22:40:06 -1000

    Create SurfSuggestion.jsx

---
+++ app/imports/ui/components/SurfSuggestion.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Grid, Header, Segment } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+
+/** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
+class SurfSuggestion extends React.Component {
+  render() {
+    return (
+      <Segment>
+        <Grid columns={3}>
+          <Grid.Column>
+            <Header as='h2'>{this.props.suggestion.name}</Header>
+          </Grid.Column>
+          <Grid.Column>
+            <Header as='h2'>{`Average Wave Height: ${this.props.suggestion.waveHeight}`}</Header>
+          </Grid.Column>
+          <Grid.Column>
+            <Header as='h2'>{`Surf Ability: ${this.props.suggestion.ability}`}</Header>
+          </Grid.Column>
+        </Grid>
+      </Segment>
+    );
+  }
+}
+
+// Require a document to be passed to this component.
+SurfSuggestion.propTypes = {
+  suggestion: PropTypes.shape({
+    name: PropTypes.string,
+    waveHeight: PropTypes.string,
+    address: PropTypes.string,
+    ability: PropTypes.number,
+    _id: PropTypes.string,
+  }).isRequired,
+};
+
+export default withRouter(SurfSuggestion);
