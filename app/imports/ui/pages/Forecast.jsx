b037cc3fc521390ae7b8c59d72d4f07a0b5ac617
ianbm <ianbm@hawaii.edu>
2021-11-16 12:22:07 -1000

    Update Forecast.jsx

--- app/imports/ui/pages/Forecast.jsx
+++ app/imports/ui/pages/Forecast.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Table, Image, Header } from 'semantic-ui-react';
+import { Container, Table, Header } from 'semantic-ui-react';
 
 export default class Forecast extends React.Component {
 
   forecastData = [
     {
       name: 'Sunset Beach',
-      image: 'https://hioceansafety.com/wp-content/uploads/2018/11/Hawaii’s-Surf-Seasons.jpg',
+      image: 'https://www.best-of-oahu.com/images/Sunset-Beach-Oahu-2.jpg',
       surf: '6-10',
       tide: '0.2',
       wind: '2 NNW',
@@ -16,7 +16,7 @@ export default class Forecast extends React.Component {
     },
     {
       name: 'Mokuleia',
-      image: 'https://hioceansafety.com/wp-content/uploads/2018/11/Hawaii’s-Surf-Seasons.jpg',
+      image: 'https://d1li3p7kp8c46b.cloudfront.net/13000/12523.4756.e64f312f51.jpg',
       surf: '5-8',
       tide: '0.2',
       wind: '8 SSW',
@@ -26,7 +26,7 @@ export default class Forecast extends React.Component {
     },
     {
       name: 'Laniakea',
-      image: 'https://hioceansafety.com/wp-content/uploads/2018/11/Hawaii’s-Surf-Seasons.jpg',
+      image: 'https://www.best-of-oahu.com/images/Laniakea-Beach-2.jpg',
       surf: '5-8',
       tide: '0.2',
       wind: '3 SSW',
@@ -36,7 +36,7 @@ export default class Forecast extends React.Component {
     },
     {
       name: 'Pipeline',
-      image: 'https://hioceansafety.com/wp-content/uploads/2018/11/Hawaii’s-Surf-Seasons.jpg',
+      image: 'https://media-cdn.tripadvisor.com/media/photo-s/16/fc/15/cf/beach.jpg',
       surf: '6-8',
       tide: '0.2',
       wind: '2 NNW',
@@ -46,7 +46,7 @@ export default class Forecast extends React.Component {
     },
     {
       name: 'Haleiwa',
-      image: 'https://hioceansafety.com/wp-content/uploads/2018/11/Hawaii’s-Surf-Seasons.jpg',
+      image: 'https://www.aloha-hawaii.com/wp-content/uploads/2010/07/haleiwa-beach-park.jpg',
       surf: '4-6',
       tide: '0.2',
       wind: '3 SSW',
@@ -56,7 +56,7 @@ export default class Forecast extends React.Component {
     },
     {
       name: 'Waimea Bay',
-      image: 'https://hioceansafety.com/wp-content/uploads/2018/11/Hawaii’s-Surf-Seasons.jpg',
+      image: 'https://www.best-of-oahu.com/images/xWaimea-Bay-Hawaii.jpg.pagespeed.ic.yfWUlSbrXr.jpg',
       surf: '6-8',
       tide: '0.2',
       wind: '2 NNW',
@@ -66,7 +66,7 @@ export default class Forecast extends React.Component {
     },
     {
       name: 'Barbers Point',
-      image: 'https://hioceansafety.com/wp-content/uploads/2018/11/Hawaii’s-Surf-Seasons.jpg',
+      image: 'https://media-cdn.tripadvisor.com/media/photo-s/16/d5/d8/fc/beach.jpg',
       surf: '2-3',
       tide: '0.9',
       wind: '8 SSE',
@@ -76,7 +76,7 @@ export default class Forecast extends React.Component {
     },
     {
       name: 'Sand Island',
-      image: 'https://hioceansafety.com/wp-content/uploads/2018/11/Hawaii’s-Surf-Seasons.jpg',
+      image: 'https://i2.wp.com/on-walkabout.net/wp-content/uploads/2018/05/Sand-Island-beach.png?fit=1200%2C555&ssl=1',
       surf: '2-3',
       tide: '0.8',
       wind: '9 S',
@@ -88,32 +88,32 @@ export default class Forecast extends React.Component {
 
   render() {
     return (
-      <Table fixed>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell/>
-            <Table.HeaderCell>Location</Table.HeaderCell>
-            <Table.HeaderCell>Surf Height</Table.HeaderCell>
-            <Table.HeaderCell>Tide</Table.HeaderCell>
-            <Table.HeaderCell>Wind</Table.HeaderCell>
-            <Table.HeaderCell>Swells</Table.HeaderCell>
-            <Table.HeaderCell>Water Temp</Table.HeaderCell>
-            <Table.HeaderCell>Weather</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {this.forecastData.map(location => <Table.Row key={location.name}>
-            <Table.Cell><Image src={location.image} size='small'/></Table.Cell>
-            <Table.Cell><Header as='h2'>{location.name}</Header></Table.Cell>
-            <Table.Cell>{location.surf}</Table.Cell>
-            <Table.Cell>{location.tide}</Table.Cell>
-            <Table.Cell>{location.wind}</Table.Cell>
-            <Table.Cell>{location.swells}</Table.Cell>
-            <Table.Cell>{location.wTemp}</Table.Cell>
-            <Table.Cell>{location.weather}</Table.Cell>
-          </Table.Row>)}
-        </Table.Body>
-      </Table>
+      <Container>
+        <Table fixed>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell><Header as='h2'>Location</Header></Table.HeaderCell>
+              <Table.HeaderCell><Header as='h2'>Surf Height</Header></Table.HeaderCell>
+              <Table.HeaderCell><Header as='h2'>Tide</Header></Table.HeaderCell>
+              <Table.HeaderCell><Header as='h2'>Wind</Header></Table.HeaderCell>
+              <Table.HeaderCell><Header as='h2'>Swells</Header></Table.HeaderCell>
+              <Table.HeaderCell><Header as='h2'>Water Temp</Header></Table.HeaderCell>
+              <Table.HeaderCell><Header as='h2'>Weather</Header></Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {this.forecastData.map(location => <Table.Row style={ { backgroundImage: `url(${(location.image)})`, backgroundSize: 'cover', height: '200px', backgroundPosition: 'center', color: 'white' } } key={location.name}>
+              <Table.Cell>{location.name}</Table.Cell>
+              <Table.Cell>{location.surf} ft</Table.Cell>
+              <Table.Cell>{location.tide} ft</Table.Cell>
+              <Table.Cell>{location.wind}</Table.Cell>
+              <Table.Cell>{location.swells}</Table.Cell>
+              <Table.Cell>{location.wTemp} &deg;F</Table.Cell>
+              <Table.Cell>{location.weather} &deg;F</Table.Cell>
+            </Table.Row>)}
+          </Table.Body>
+        </Table>
+      </Container>
     );
   }
 }
