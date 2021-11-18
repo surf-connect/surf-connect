3be39d8113a5de14378d223a7f162c02ff249e0a
Jonathan-Ma <jma66@hawaii.edu>
2021-11-18 11:16:51 -1000

    landing intro

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -5,14 +5,14 @@ import { Container, Grid, Header } from 'semantic-ui-react';
 class Landing extends React.Component {
   render() {
     return (
-      <Container textAlign={'center'}>
+      <Container textAlign={'center'} >
         <Header id={'landing-font'} as={'h1'}>Welcome to Surf Connect!</Header>
         <br/>
         <br/>
-        <Grid columns={3}>
+        <Grid columns={3} >
           <Grid.Column/>
           <Grid.Column>
-            <Header as={'h3'} id={'intro'}>Are you new to surfing and don&apos;t know where to start? Or are you just looking for a surfing buddy to try out a new spot? Use Surf Connect to link with other surfers who can teach you how to surf or who are also looking for a surfing buddy, or both!</Header>
+            <Header as={'h3'} id={'root'} >Are you new to surfing and don&apos;t know where to start? Or are you just looking for a surfing buddy to try out a new spot? Use Surf Connect to link with other surfers who can teach you how to surf or who are also looking for a surfing buddy!</Header>
           </Grid.Column>
           <Grid.Column/>
         </Grid>
