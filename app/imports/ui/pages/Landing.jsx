a538487e387df887aa4be543a871882cae3d599c
Jonathan-Ma <jma66@hawaii.edu>
2021-11-18 11:38:39 -1000

    Landing done need more design

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Container, Grid, Header } from 'semantic-ui-react';
+import { Button, Container, Grid, Header } from 'semantic-ui-react';
+import { NavLink } from 'react-router-dom';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
@@ -9,13 +10,28 @@ class Landing extends React.Component {
         <Header id={'landing-font'} as={'h1'}>Welcome to Surf Connect!</Header>
         <br/>
         <br/>
-        <Grid columns={3} >
-          <Grid.Column/>
-          <Grid.Column>
-            <Header as={'h3'} id={'root'} >Are you new to surfing and don&apos;t know where to start? Or are you just looking for a surfing buddy to try out a new spot? Use Surf Connect to link with other surfers who can teach you how to surf or who are also looking for a surfing buddy!</Header>
-          </Grid.Column>
-          <Grid.Column/>
-        </Grid>
+        <Grid.Row>
+          <Grid columns={3}>
+            <Grid.Column/>
+            <Grid.Column>
+              {/* eslint-disable-next-line max-len */}
+              <Header as={'h3'} id={'root'} >Are you new to surfing and don&apos;t know where to start? Or are you just looking for a surfing buddy to try out a new spot? Use Surf Connect to link with other surfers who can teach you how to surf or who are also looking for a surfing buddy!</Header>
+            </Grid.Column>
+            <Grid.Column/>
+          </Grid>
+        </Grid.Row>
+        <br/>
+        <br/>
+        <Grid.Row>
+          <Grid columns={3}>
+            <Grid.Column/>
+            <Grid.Column>
+              <Button as={NavLink} to={'/signup'}>Sign Up</Button>
+              <Button as={NavLink} to={'/signin'}>Sign In</Button>
+            </Grid.Column>
+            <Grid.Column/>
+          </Grid>
+        </Grid.Row>
       </Container>
     );
   }
