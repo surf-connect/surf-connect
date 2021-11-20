ad2e9ad0201eaf447a9edcdbcac5814d5b04cbcd
charlie-robison <charlierobison480@gmail.com>
2021-11-19 15:27:01 -1000

    Update

--- app/imports/ui/pages/Connect.jsx
+++ app/imports/ui/pages/Connect.jsx
@@ -19,7 +19,7 @@ const formSchema = new SimpleSchema({
   time: {
     type: String,
     allowedValues: ['12:00am', '1:00am', '2:00am', '3:00am', '4:00am', '5:00am', '6:00am', '7:00am', '8:00am', '9:00am',
-      '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', '8:00pm', '9:00pm', '10:00pm', '11:00pm', '12:00am'],
+      '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', '8:00pm', '9:00pm', '10:00pm', '11:00pm'],
     defaultValue: '12:00am',
   },
 });
@@ -100,7 +100,7 @@ class Connect extends React.Component {
         <div style={messageStyle}>
           <Dropdown text='Messages' icon='chat' floating labeled button className='icon'>
             <Dropdown.Menu>
-              {this.messages.map(message => <Message key={message.message} message={message} />)}
+              {this.props.messages.map(message => <Message key={message._id} message={message} />)}
             </Dropdown.Menu>
           </Dropdown>
         </div>
@@ -134,6 +134,7 @@ Connect.propTypes = {
 export default withTracker(() => {
   // Get access to Stuff documents.
   const subscription = Meteor.subscribe(Messages.userPublicationName);
+  console.log(`Username: ${Messages.userPublicationName}`);
   // Determine if the subscription is ready
   const ready = subscription.ready();
   // Get the Message documents
