ed522ee983dd7eed5335a1e55b06e52b4a7ba47f
Jonathan-Ma <jma66@hawaii.edu>
2021-11-23 10:58:18 -1000

    location schema

--- app/imports/api/location/Message.js
+++ app/imports/api/location/Message.js
@@ -2,18 +2,21 @@ import { Mongo } from 'meteor/mongo';
 import SimpleSchema from 'simpl-schema';
 import { Tracker } from 'meteor/tracker';
 
-class MessageCollection {
+class LocationCollection {
   constructor() {
     // The name of this collection.
-    this.name = 'MessageCollection';
+    this.name = 'LocationCollection';
     // Define the Mongo collection.
     this.collection = new Mongo.Collection(this.name);
     // Define the structure of each document in the collection.
     this.schema = new SimpleSchema({
-      sender: String,
-      receiver: String,
-      image: String,
-      message: String,
+      location: String,
+      surfHeight: Number,
+      tide: Number,
+      wind: Number,
+      swells: Number,
+      waterTemp: Number;
+      weather: Number,
     }, { tracker: Tracker });
     // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
     this.collection.attachSchema(this.schema);
@@ -23,4 +26,4 @@ class MessageCollection {
   }
 }
 
-export const Messages = new MessageCollection();
+export const Locations = new LocationCollection();
