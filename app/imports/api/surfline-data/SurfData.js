c27fee77cadea8b99bb33ceafc1ab6594283de5e
charlie-robison <charlierobison480@gmail.com>
2021-12-02 21:45:15 -1000

    Created API calls for each location.

--- app/imports/api/surfline-data/SurfData.js
+++ app/imports/api/surfline-data/SurfData.js
@@ -1,8 +1,9 @@
 import { fetch } from 'meteor/fetch';
 
-export const surfData = async () => {
-  const response = await fetch('https://services.surfline.com/kbyg/spots/forecasts/wave?spotId=5842041f4e65fad6a770888d&days=1&intervalHours=24&maxHeights=false&sds=false');
+export const surfData = async (spotId, spotName) => {
+  const response = await fetch(`https://services.surfline.com/kbyg/spots/forecasts/wave?spotId=${spotId}&days=1&intervalHours=24&maxHeights=false&sds=false`);
   const toJSON = await response.json();
-  console.log(`Object:${JSON.stringify(toJSON)}!!!!!`);
-  console.log(`Wave Height: ${toJSON.data.wave[0].surf.min} - ${toJSON.data.wave[0].surf.max}`);
+  const minWaveHeight = Math.floor(toJSON.data.wave[0].surf.min);
+  const maxWaveHeight = Math.floor(toJSON.data.wave[0].surf.max);
+  console.log(`${spotName}: ${minWaveHeight}-${maxWaveHeight} ft`);
 };
--- app/imports/startup/server/Startup.js
+++ app/imports/startup/server/Startup.js
@@ -2,5 +2,13 @@ import { Meteor } from 'meteor/meteor';
 import { surfData } from '../../api/surfline-data/SurfData';
 
 Meteor.startup(() => {
-  surfData();
+  surfData('5842041f4e65fad6a770888d', 'Sunset Beach');
+  surfData('5842041f4e65fad6a7708e30', 'Sand Island');
+  surfData('5842041f4e65fad6a7708df5', 'Haleiwa');
+  surfData('5842041f4e65fad6a7708898', 'Laniakea');
+  surfData('5842041f4e65fad6a7708df8', 'Mokuleia');
+  surfData('5842041f4e65fad6a7708dfe', 'Barbers Point');
+  surfData('5842041f4e65fad6a7708895', 'Waimea Bay');
+  surfData('5842041f4e65fad6a7708890', 'Pipeline');
+  surfData('5842041f4e65fad6a7708df6', 'Sandy Beach');
 });
