99c4271cb251d9813e33f6e3fb3ef1965546c221
charlie-robison <charlierobison480@gmail.com>
2021-12-02 20:58:18 -1000

    Wave Heights Obtained

--- app/imports/api/surfline-data/SurfData.js
+++ app/imports/api/surfline-data/SurfData.js
@@ -1,8 +1,8 @@
 import { fetch } from 'meteor/fetch';
 
 export const surfData = async () => {
-  const response = await fetch('https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?begin_date=20130808 15:00&end_date=20130808 15:06&station=1612340&product=hourly_height&datum=HWI&units=english&time_zone=gmt&application=ports_screen&format=json');
+  const response = await fetch('https://services.surfline.com/kbyg/spots/forecasts/wave?spotId=5842041f4e65fad6a770888d&days=1&intervalHours=24&maxHeights=false&sds=false');
   const toJSON = await response.json();
   console.log(`Object:${JSON.stringify(toJSON)}!!!!!`);
-  console.log(`Location: ${toJSON.metadata.name}`);
+  console.log(`Wave Height: ${toJSON.data.wave[0].surf.min} - ${toJSON.data.wave[0].surf.max}`);
 };
