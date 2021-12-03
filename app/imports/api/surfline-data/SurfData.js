11c226255b156e014ce6fcfde7c5d3c07bbc0fc8
charlie-robison <charlierobison480@gmail.com>
2021-12-02 22:58:04 -1000

    All data for all locations are now connected to Surflines realtime api.

--- app/imports/api/surfline-data/SurfData.js
+++ app/imports/api/surfline-data/SurfData.js
@@ -1,9 +1,52 @@
 import { fetch } from 'meteor/fetch';
+import { Locations } from '../location/Location';
 
-export const surfData = async (spotId, spotName) => {
-  const response = await fetch(`https://services.surfline.com/kbyg/spots/forecasts/wave?spotId=${spotId}&days=1&intervalHours=24&maxHeights=false&sds=false`);
-  const toJSON = await response.json();
-  const minWaveHeight = Math.floor(toJSON.data.wave[0].surf.min);
-  const maxWaveHeight = Math.floor(toJSON.data.wave[0].surf.max);
-  console.log(`${spotName}: ${minWaveHeight}-${maxWaveHeight} ft`);
+const calculateAbility = (minWaveHeight, maxWaveHeight) => {
+  const averageWaveHeight = (maxWaveHeight + minWaveHeight)/2;
+  const ability = Math.floor(averageWaveHeight / 1.3);
+  if (ability <= 0) {
+    return 1;
+  } else if (ability > 5) {
+    return 5;
+  }
+  return ability;
+};
+
+
+export const surfData = async (spotId, spotName, spotImage) => {
+  const waveInfo = await fetch(`https://services.surfline.com/kbyg/spots/forecasts/wave?spotId=${spotId}&days=1&intervalHours=24&maxHeights=false&sds=false`);
+  const waveInfoToJson = await waveInfo.json();
+  const minWaveHeight = Math.floor(waveInfoToJson.data.wave[0].surf.min);
+  const maxWaveHeight = Math.floor(waveInfoToJson.data.wave[0].surf.max);
+  const swellHeight = waveInfoToJson.data.wave[0].swells[0].height;
+
+  const windInfo = await fetch(`https://services.surfline.com/kbyg/spots/forecasts/wind?spotId=${spotId}&days=1&intervalHours=24&maxHeights=false&sds=false`);
+  const windInfoToJson = await windInfo.json();
+  const windSpeed = windInfoToJson.data.wind[0].speed;
+
+  const tideInfo = await fetch(`https://services.surfline.com/kbyg/spots/forecasts/tides?spotId=${spotId}&days=1&intervalHours=24&maxHeights=false&sds=false`);
+  const tideInfoToJson = await tideInfo.json();
+  const tide = tideInfoToJson.data.tides[0].height;
+
+  const weatherInfo = await fetch(`https://services.surfline.com/kbyg/spots/forecasts/weather?spotId=${spotId}&days=1&intervalHours=24&maxHeights=false&sds=false`);
+  const weatherInfoToJson = await weatherInfo.json();
+  const temp = weatherInfoToJson.data.weather[0].temperature;
+
+  const ability = calculateAbility(minWaveHeight, maxWaveHeight);
+
+  const location = {
+    name: spotName,
+    image: spotImage,
+    surf: `${minWaveHeight}-${maxWaveHeight} ft`,
+    tide: tide,
+    wind: windSpeed,
+    swells: swellHeight,
+    weather: temp,
+    ability: ability,
+  };
+  console.log(JSON.stringify(location));
+  Locations.collection.insert(location);
+  console.log(`Inserted location: ${spotName} to DB.`);
+  console.log(`Location data: ${JSON.stringify(location)}`);
+  // console.log(`${spotName}: ${minWaveHeight}-${maxWaveHeight} ft`);
 };
--- app/imports/startup/server/Startup.js
+++ app/imports/startup/server/Startup.js
@@ -2,13 +2,13 @@ import { Meteor } from 'meteor/meteor';
 import { surfData } from '../../api/surfline-data/SurfData';
 
 Meteor.startup(() => {
-  surfData('5842041f4e65fad6a770888d', 'Sunset Beach');
-  surfData('5842041f4e65fad6a7708e30', 'Sand Island');
-  surfData('5842041f4e65fad6a7708df5', 'Haleiwa');
-  surfData('5842041f4e65fad6a7708898', 'Laniakea');
-  surfData('5842041f4e65fad6a7708df8', 'Mokuleia');
-  surfData('5842041f4e65fad6a7708dfe', 'Barbers Point');
-  surfData('5842041f4e65fad6a7708895', 'Waimea Bay');
-  surfData('5842041f4e65fad6a7708890', 'Pipeline');
-  surfData('5842041f4e65fad6a7708df6', 'Sandy Beach');
+  surfData('5842041f4e65fad6a770888d', 'Sunset Beach', 'https://www.best-of-oahu.com/images/Sunset-Beach-Oahu-2.jpg');
+  surfData('5842041f4e65fad6a7708e30', 'Sand Island', 'https://i2.wp.com/on-walkabout.net/wp-content/uploads/2018/05/Sand-Island-beach.png?fit=1200%2C555&ssl=1');
+  surfData('5842041f4e65fad6a7708df5', 'Haleiwa', 'https://www.aloha-hawaii.com/wp-content/uploads/2010/07/haleiwa-beach-park.jpg');
+  surfData('5842041f4e65fad6a7708898', 'Laniakea', 'https://www.best-of-oahu.com/images/Laniakea-Beach-2.jpg');
+  surfData('5842041f4e65fad6a7708df8', 'Mokuleia', 'https://d1li3p7kp8c46b.cloudfront.net/13000/12523.4756.e64f312f51.jpg');
+  surfData('5842041f4e65fad6a7708dfe', 'Barbers Point', 'https://media-cdn.tripadvisor.com/media/photo-s/16/d5/d8/fc/beach.jpg');
+  surfData('5842041f4e65fad6a7708895', 'Waimea Bay', 'https://www.best-of-oahu.com/images/xWaimea-Bay-Hawaii.jpg.pagespeed.ic.yfWUlSbrXr.jpg');
+  surfData('5842041f4e65fad6a7708890', 'Pipeline', 'https://media-cdn.tripadvisor.com/media/photo-s/16/fc/15/cf/beach.jpg');
+  surfData('5842041f4e65fad6a7708df6', 'Sandy Beach', 'https://www.fodors.com/assets/destinations/58227/sandy-beach-honolulu-and-oahu-oahu-hawaii-usa_main.jpg');
 });
