2c0d7a8534a20d1724cfdd44e06e1ae73cb5051e
charlie-robison <charlierobison480@gmail.com>
2021-12-14 21:58:25 -1000

    Updated Forecast UI for easier viewing

--- app/imports/ui/pages/Forecast.jsx
+++ app/imports/ui/pages/Forecast.jsx
@@ -13,28 +13,31 @@ class Forecast extends React.Component {
 
   renderPage() {
     const style = { marginTop: '50px', fontFamily: 'Original Surfer, cursive' };
+    const headerStyle = {
+      WebkitTextStroke: '1px black',
+    };
     return (
       <Container id='forecast-page'>
         <Header as='h1' style={style} textAlign='center'>Oahu Surf Forecast</Header>
         <Table fixed inverted size='large'>
           <Table.Header>
             <Table.Row>
-              <Table.HeaderCell>Location</Table.HeaderCell>
-              <Table.HeaderCell>Surf Height</Table.HeaderCell>
-              <Table.HeaderCell>Wind</Table.HeaderCell>
-              <Table.HeaderCell>Temperature</Table.HeaderCell>
-              <Table.HeaderCell>Weather</Table.HeaderCell>
-              <Table.HeaderCell>Ability</Table.HeaderCell>
+              <Table.HeaderCell><Header as='h2' inverted>Location</Header></Table.HeaderCell>
+              <Table.HeaderCell><Header as='h2' inverted>Surf Height</Header></Table.HeaderCell>
+              <Table.HeaderCell><Header as='h2' inverted>Wind</Header></Table.HeaderCell>
+              <Table.HeaderCell><Header as='h2' inverted>Temperature</Header></Table.HeaderCell>
+              <Table.HeaderCell><Header as='h2' inverted>Weather</Header></Table.HeaderCell>
+              <Table.HeaderCell><Header as='h2' inverted>Ability</Header></Table.HeaderCell>
             </Table.Row>
           </Table.Header>
           <Table.Body>
             {this.props.locations.map(location => <Table.Row style={ { backgroundImage: `url(${(location.image)})`, backgroundSize: 'cover', height: '100px', backgroundPosition: 'center' } } key={location.name}>
-              <Table.Cell>{location.name}</Table.Cell>
-              <Table.Cell>{location.surf}</Table.Cell>
-              <Table.Cell>{location.wind}</Table.Cell>
-              <Table.Cell>{location.temperature} &deg;F</Table.Cell>
-              <Table.Cell>{location.weather} ft</Table.Cell>
-              <Table.Cell>{location.ability}</Table.Cell>
+              <Table.Cell><Header as='h3' style={headerStyle} inverted>{location.name}</Header></Table.Cell>
+              <Table.Cell><Header as='h3' style={headerStyle} inverted>{location.surf}</Header></Table.Cell>
+              <Table.Cell><Header as='h3' style={headerStyle} inverted>{location.wind}</Header></Table.Cell>
+              <Table.Cell><Header as='h3' style={headerStyle} inverted>{location.temperature} &deg;F</Header></Table.Cell>
+              <Table.Cell><Header as='h3' style={headerStyle} inverted>{location.weather}</Header></Table.Cell>
+              <Table.Cell><Header as='h3' style={headerStyle} inverted>{location.ability}</Header></Table.Cell>
             </Table.Row>)}
           </Table.Body>
         </Table>
