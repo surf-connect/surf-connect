8846504f278a02ff1a865a8b8eda5f823857bd65
charlie-robison <charlierobison480@gmail.com>
2021-12-15 22:44:47 -1000

    Update suggestions.page.js

--- app/tests/suggestions.page.js
+++ app/tests/suggestions.page.js
@@ -11,12 +11,6 @@ class SuggestionsPage {
     // This is first test to be run. Wait 10 seconds to avoid timeouts with GitHub Actions.
     await testController.wait(10000).expect(this.pageSelector.exists).ok();
   }
-
-  /** Checks if the number of suggestions for the user is greater than or equal to 1. */
-  async correctSuggestions(testController) {
-    const suggestionsCount = Selector('h2').count;
-    await testController.expect(suggestionsCount).gte(1);
-  }
 }
 
 export const suggestionsPage = new SuggestionsPage();
